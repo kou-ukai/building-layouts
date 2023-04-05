84c232954196f6328a1d7223c903b5105da3d86a
kou.ukai <kou.ukai@cisys.co.jp>
2023-04-05 22:13:27 +0900

    下部のボタン領域作成

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'Call'),
+        _buildButtonColumn(color, Icons.near_me, 'Route'),
+        _buildButtonColumn(color, Icons.share, 'Share')
+      ],
+    );
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
@@ -39,8 +48,29 @@ class MyApp extends StatelessWidget {
       title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(title: const Text('Flutter layout demo')),
-        body: Column(children: [titleSection]),
+        body: Column(children: [titleSection, buttonSection]),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          color: color,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+                fontSize: 12, fontWeight: FontWeight.w400, color: color),
+          ),
+        )
+      ],
     );
   }
 }
