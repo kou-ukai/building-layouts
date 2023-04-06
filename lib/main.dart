acab2fec075d9c4a643444707a2a8e52879ee6e9
kou.ukai <kou.ukai@cisys.co.jp>
2023-04-06 23:59:14 +0900

    状態管理によるボタンアクション追加

--- lib/main.dart
+++ lib/main.dart
@@ -48,11 +48,7 @@ class MyApp extends StatelessWidget {
               )
             ],
           )),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
+          const FavoriteWidget()
         ],
       ),
     );
@@ -93,6 +89,51 @@ class MyApp extends StatelessWidget {
                 fontSize: 12, fontWeight: FontWeight.w400, color: color),
           ),
         )
+      ],
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FavotiteWidgetState();
+  }
+}
+
+class _FavotiteWidgetState extends State<FavoriteWidget>{
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState((){
+      if(_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon:(_isFavorited ? const Icon(Icons.star): const Icon(Icons.star_border)),
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(width: 18, child: SizedBox(child: Text('$_favoriteCount'),),)
       ],
     );
   }
